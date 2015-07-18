6562a4eb9405ce349d5208733da24527ef2aba21
fotis <fotis@fotis.usersys.redhat.com>
2015-07-18 13:29:53 -0400

    rename

---
+++ winmesg.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+""" Popup messages """
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Fotios Tsiadimos
+# Licensed under the terms of the GPL License
+# (see License file for details)
+
+
+import wx
+import wx.lib.agw.toasterbox as TB
+popdefault = 1
+
+def toster(self, event):
+        toaster = TB.ToasterBox(self, tbstyle=TB.TB_SIMPLE)
+        toaster.SetTitle('NetSpy2Ban')
+        toaster.SetPopupPauseTime(4000)
+        toaster.SetPopupBackgroundColour("BLACK")
+        toaster.SetPopupPositionByInt(0)
+        toaster.SetPopupScrollSpeed(10)
+        toaster.SetPopupSize((400,100))
+        toaster.SetPopupTextColour("RED")
+        toaster.SetPopupText(event)
+	if popdefault == 1:
+        	wx.CallLater(1000, toaster.Play)
+
+
+if __name__ == "__main__":
+    print "This is a module for netspy2ban.py"
+
